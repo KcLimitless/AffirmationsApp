df4e6418e6b32806138deaabcead4a288491eed6
KelechiDanAndroid <kcfordskys@yahoo.com>
2021-07-01 08:28:31 +0200

    first commit

---
+++ app/src/main/java/com/e/affirmations/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.e.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.e.affirmations.adapter.ItemAdapter
+import com.e.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        //recyclerView.layoutManager = GridLayoutManager(this, 3)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+    }
+}
